d2b732011bda381b0b6eb22da438c6ce3fc99eae
crseguraoliva <crseguraoliva@gmail.com>
2026-05-14 23:20:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const { createClient } = require("@supabase/supabase-js");
 const cron = require("node-cron");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 const supabase = createClient(
